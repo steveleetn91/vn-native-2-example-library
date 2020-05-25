95cdc2c7beb3a526b0fdc8f9542230c53c197893
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-25 21:41:11 +0700

    Testing

--- src/ios/Echo.h
+++ src/ios/Echo.h
@@ -1,20 +1,7 @@
-#import "Echo.h"
 #import <Cordova/CDVPlugin.h>
 
-@implementation Echo
+@interface Echo : CDVPlugin
 
-- (void)echo:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
+- (void)echo:(CDVInvokedUrlCommand*)command;
 
 @end
