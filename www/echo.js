cc38ccc56a85cf6d9852d8b07e4b3296e697daf2
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-26 00:54:27 +0700

    Testing

--- www/echo.js
+++ www/echo.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 module.exports = {
     run :  function(str,callback){
-        cordova.exec(callback, function(err) {
+        exec(callback, function(err) {
             callback(err);
         }, "Echo", "echo", [str]);
     }
