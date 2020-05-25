e5b0318c59eca558b15cf0370c88d9425b72bb9e
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-05-25 21:46:25 +0700

    Testing

--- www/example-library.js
+++ www/example-library.js
@@ -1,4 +1,4 @@
-window.echo = function(str, callback) {
+module.exports = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "Echo", "echo", [str]);
